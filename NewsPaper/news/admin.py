967d0ba4ecd04382514260bd7435f01541955017
Evgeniy Korsakov <evgeniy.korsakov@hostwaycorp.com>
2023-04-28 21:27:01 +0600

    d11

--- NewsPaper/news/admin.py
+++ NewsPaper/news/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 
 from .models import Category, Post
 
+
+class PostAdmin(admin.ModelAdmin):
+    # list_display - это список или кортеж со всеми полями, которые вы хотите видеть в таблице с товарами
+    list_display = ('title', 'post_type', 'rating', 'creation_date')
+    list_filter = ('title', 'post_type', 'rating', 'creation_date')
+    search_fields = ('title', 'post_category__name')
+
+
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
